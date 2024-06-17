a6700f54e007eb27290e5cb2eb183a0690d6dcc5
Abhishek Sah <abhi.sah.97@gmail.com>
2024-06-17 19:28:34 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Abhishek Sah on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
